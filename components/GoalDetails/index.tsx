d52047cdd23bcf0da29fe9b7d0c8b203d5d7fbcd
Adam Richard Turner <hello@adamrichardturner.dev>
2024-12-22 03:45:53 +0000

    Fix padding

--- components/GoalDetails/index.tsx
+++ components/GoalDetails/index.tsx
@@ -71,7 +71,7 @@ export default function GoalDetails({ goal }: GoalDetailsProps) {
       transition={{ duration: 0.5 }}
       className='min-h-screen bg-background'
     >
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
+      <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
         <div className='space-y-4'>
           <GoalBanner goal={goal} />
 
