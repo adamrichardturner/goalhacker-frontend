db577b8908388a52e588d30f6d9418a22ccc7f8b
Adam Richard Turner <hello@adamrichardturner.dev>
2024-12-22 07:13:21 +0000

    Padding bottom

--- components/GoalDetails/index.tsx
+++ components/GoalDetails/index.tsx
@@ -69,7 +69,7 @@ export default function GoalDetails({ goal }: GoalDetailsProps) {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className='min-h-screen bg-background'
+      className='min-h-screen bg-background pb-12'
     >
       <div className='max-w-7xl mx-auto'>
         <div className='space-y-4'>
