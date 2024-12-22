4689d859fc4844026ba7d6682c7b6a93ac915e0b
Adam Richard Turner <hello@adamrichardturner.dev>
2024-12-22 04:20:52 +0000

    Style improvements

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -23,8 +23,8 @@ function GoalsContent() {
   return (
     <div className='container max-w-3xl flex flex-col gap-6 sm:px-4 w-full'>
       <Header user={user} />
-      <div className='flex flex-col gap-4 w-full rounded-lg'>
-        <main className='flex flex-col gap-4 w-full bg-card px-4 py-6 sm:px-12 sm:py-12 rounded-lg shadow-sm'>
+      <div className='flex flex-col gap-4 w-full rounded-lg mb-6'>
+        <main className='flex flex-col gap-4 w-full bg-card px-4 py-6 mb-6 sm:px-12 sm:py-12 rounded-lg shadow-sm'>
           <GoalsView goals={goals} user={user} isLoading={goalsLoading} />
         </main>
       </div>
