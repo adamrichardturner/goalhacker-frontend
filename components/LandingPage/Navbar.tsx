c18b8a9d9234a3ab06414fdd7d049dda36cea247
Robert P <porobertdev@gmail.com>
2025-01-19 20:04:14 +0200

    Remove animation in the navbar
    We're going to bring attention to the hero
    with a stagger effect on the 3 cards.

--- components/LandingPage/Navbar.tsx
+++ components/LandingPage/Navbar.tsx
@@ -1,22 +1,12 @@
 import Link from 'next/link'
 import Logo from '../Logo'
-import { motion } from 'framer-motion'
 import { Button } from '../ui/button'
 
 const links = ['Features', 'FAQ']
 
 const Navbar = () => {
   return (
-    <motion.div
-      className='flex justify-between items-center'
-      initial={{ scale: 0.5, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{
-        type: 'spring',
-        stiffness: 200,
-        damping: 20,
-      }}
-    >
+    <div className='flex justify-between items-center'>
       <Logo size='2xl' mode='dark' />
       {/* links */}
       <div className='text-white flex gap-[26px] items-center'>
@@ -29,7 +19,7 @@ const Navbar = () => {
           Get started
         </Button>
       </div>
-    </motion.div>
+    </div>
   )
 }
 
