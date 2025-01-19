16303f9e8b77b7b2bd9a53391cf2f419794e2484
Robert P <porobertdev@gmail.com>
2025-01-19 21:49:08 +0200

    Add stagger effect in hero section

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -1,6 +1,28 @@
+import { motion } from 'framer-motion'
 import { Button } from '../ui/button'
 
+const images = [
+  '/mockups/goalhacker-hero-1.jpg',
+  '/mockups/goalhacker-hero-2.jpg',
+  '/mockups/goalhacker-hero-3.jpg',
+]
+
+const variants = {
+  hidden: { opacity: 0, y: -50 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.3,
+    },
+    y: 0,
+  },
+}
+
 const Hero = () => {
+  const overlapDistance = 80 // Distance for vertical overlap
+  const horizontalShift = 50 // Distance for horizontal shift
+
   return (
     <div className='flex min-h-screen gap-[98] text-white mt-[80]'>
       <div className='flex flex-col gap-[35] w-1/2'>
@@ -19,11 +41,31 @@ const Hero = () => {
         </Button>
       </div>
 
-      <img
-        src='/mockups/goalhacker-hero.svg'
-        alt='Goal Hacker Dashboard'
-        className='w-1/2 h-1/2 object-cover object-top'
-      />
+      {/* REF: https://www.youtube.com/watch?v=6YOPRk8mTn0 */}
+      <motion.div
+        variants={variants}
+        className='w-1/2 h-1/2'
+        initial='hidden'
+        animate='visible'
+      >
+        <div className='img-wrapper relative'>
+          {images.map((img, index) => {
+            return (
+              <motion.div
+                variants={variants}
+                key={img}
+                style={{
+                  position: 'absolute',
+                  top: index * overlapDistance, // Offset each image down
+                  left: index * horizontalShift, // Offset each image right
+                }}
+              >
+                <img src={img} alt='Goal Hacker' className='rounded-[25]' />
+              </motion.div>
+            )
+          })}
+        </div>
+      </motion.div>
     </div>
   )
 }
