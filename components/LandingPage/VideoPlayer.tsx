932b48ed2ddaafa97aa111c7d1f7f5493e617ac0
Adam Richard Turner <hello@adamrichardturner.dev>
2025-01-24 23:25:30 +0000

    Video showcase

---
+++ components/LandingPage/VideoPlayer.tsx
@@ -0,0 +1,11 @@
+const demoSrc = "https://goalhacker-video.s3.eu-west-2.amazonaws.com/goalhacker-screen.mp4"
+
+const VideoPlayer = () => {
+  return (
+    <div className='flex justify-center items-center w-full'>
+      <video src={demoSrc} height={500} width={500} autoPlay muted loop className="rounded-lg w-full" />
+    </div>
+  )
+}
+
+export default VideoPlayer
